dca009ec6cd3396e070108600044a56dbf0cf01a
Freddy Velarde <freddyvelarde59@gmail.com>
2023-01-15 10:09:03 -0400

    :zap: adding goTo definition in lsp

--- .config/nvim/lua/config/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lsp/lspconfig.lua
@@ -21,6 +21,7 @@ local on_attach = function(client, bufnr)
 
 	-- set keybinds
 	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	keymap.set("n", "gh", "<cmd>Lspsaga goto_definition<CR>", opts) -- show definition, references
 	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
 	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation

622359cd0f57b161987b97e5905071976191fe50
Freddy Velarde <freddyvelarde59@gmail.com>
2023-01-15 09:51:44 -0400

    :art: adding more features

--- .config/nvim/lua/config/core/options.lua
+++ .config/nvim/lua/config/core/options.lua
@@ -24,20 +24,13 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
--- appearance
-
 -- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
--- opt.showcmd = false
--- clipboard
--- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right

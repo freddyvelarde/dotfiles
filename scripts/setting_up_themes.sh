7f7a9c483570c896d8707555d18de0a60e363011
freddyvelarde <freddyvelarde59@gmail.com>
2023-06-25 17:19:46 -0400

    Fixing script for setting fonts up

--- scripts/setting_up_themes.sh
+++ scripts/setting_up_themes.sh
@@ -12,6 +12,7 @@
 
 ## Setting up themes
 mkdir utils
+sudo mkdir -p /usr/share/themes /usr/share/icons/ 
 cd utils
 wget -c $(curl -s https://api.github.com/repos/archcraft-os/pkgs/contents/x86_64/archcraft-gtk-theme-sweet-1.0-4-any.pkg.tar.zst | jq -r '.download_url')
 wget -c $(curl -s https://api.github.com/repos/archcraft-os/pkgs/contents/x86_64/archcraft-gtk-theme-cyberpunk-1.0-4-any.pkg.tar.zst | jq -r '.download_url')
@@ -31,5 +32,13 @@ tar -xvf archcraft-icons-luna-1.0-5-any.pkg.tar.zst
 tar -xvf archcraft-cursor-qogirr-1.0-4-any.pkg.tar.zst
 tar -xvf archcraft-cursor-fluent-1.0-4-any.pkg.tar.zst
 sudo mv usr/share/icons/* /usr/share/icons
+
+## Setting fonts up
+
+wget -c $(curl -s https://api.github.com/repos/archcraft-os/pkgs/contents/x86_64/archcraft-fonts-1.0-6-any.pkg.tar.zst | jq -r '.download_url')
+tar -xvf archcraft-fonts-1.0-6-any.pkg.tar.zst
+
+sudo mv usr/share/fonts/* /usr/share/fonts/
+
 cd ..
 rm -rf utils

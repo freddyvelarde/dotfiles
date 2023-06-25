6aa4fa1a18fee31139e1d7bb24e3772d6a29289b
freddyvelarde <freddyvelarde59@gmail.com>
2023-06-25 17:27:42 -0400

    Adding setup scripts

---
+++ scripts/node_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# nvm ls-remote --lts
+nvm install 18.16.1
--- scripts/setting_up_themes.sh
+++ scripts/setting_up_themes.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# echo "Installing rust:"
-#
-# sudo pacman -Rsn rust
-# sudo pacman -Rsn rustup
-# sudo pacman -Rsn rustc
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-#
-# source "$HOME/.cargo/env"
-#
-
 ## Setting up themes
 mkdir utils
 sudo mkdir -p /usr/share/themes /usr/share/icons/ 

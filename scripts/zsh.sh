70e84dd47a5b183afa31e17dfbd08b9ca906d2b4
freddyvelarde <freddyvelarde59@gmail.com>
2023-06-26 12:21:00 -0400

    Adding zsh script setter

---
+++ scripts/zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc

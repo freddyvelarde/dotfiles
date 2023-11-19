2c973ba21b43d5e20de116e77279f16734e9e500
freddyvelarde <freddvel.dev@gmail.com>
2023-11-19 17:42:53 -0400

    some changes

--- cfg/nvim/init.lua
+++ cfg/nvim/init.lua
@@ -2,8 +2,8 @@
 require("config.core") -- my own config
 require("config.lazy") -- plugins config
 
--- require("config.set-colorscheme") -- colorscheme
+require("config.set-colorscheme") -- colorscheme
 require("config.languages-conf") -- languages config for lsp
 
 -- set colorscheme
-vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("catppuccin-mocha")

c37c01788ad6e22fe9cd26b3de2809581cbe4771
freddyvelarde <freddvel.dev@gmail.com>
2025-03-04 17:40:38 -0400

    adding configs for `alacritty` and `ghostty` terminals

--- cfg/nvim/lua/config/colorschemes/catppuccin.lua
+++ cfg/nvim/lua/config/colorschemes/catppuccin.lua
@@ -12,8 +12,8 @@ require("catppuccin").setup({
 		shade = "dark",
 		percentage = 0.15, -- percentage of the shade to apply to the inactive window
 	},
-	no_italic = false, -- Force no italic
-	no_bold = false, -- Force no bold
+	no_italic = true, -- Force no italic
+	no_bold = true, -- Force no bold
 	no_underline = false, -- Force no underline
 	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 		comments = { "italic" }, -- Change the style of comments
